bcf5fc86dae19cf276d61976c2810d2b3bc4c43e
admin <kattubadi426@gmail.com>
2025-04-19 15:17:11 +0530

    New commit

---
+++ runner.py
@@ -0,0 +1,21 @@
+import pytest
+import sys
+
+def main():
+    # Customize the arguments for pytest run
+    pytest_args = [
+        "tests/table5",                      # Folder containing your test cases
+        "-v",                          # Verbose output
+        "--tb=short",                  # Short traceback format
+        "--capture=tee-sys",          # Capture and also print stdout/stderr
+        "--html=reports/report.html", # HTML report generation
+        "--self-contained-html",      # Standalone HTML with styles/scripts
+        "-m", "not ignore",           # Run all tests except those marked with @pytest.mark.ignore
+    ]
+
+    # Exit with pytest's exit code
+    sys.exit(pytest.main(pytest_args))
+
+
+if __name__ == "__main__":
+    main()
